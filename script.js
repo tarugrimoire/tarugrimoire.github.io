695596d4761da3eb222143024fd21028475f1355
taru <taruru1010@gmail.com>
2020-11-06 14:57:01 +0900

    fix script.js

--- script.js
+++ script.js
@@ -9,13 +9,11 @@ request.onload = function () {
   var api_resp = this.response;
   for (let i = 0; i < api_resp.length; i++) {
       const data = api_resp[i];
-      console.log(data)
       if (data.name == "tarugrimoire.github.io") {
           continue
       }
-      writeups_element.innerHTML += `<p class="writeup"><a href=${data.html_url}>${data.name}</a></p>`
-      
+      writeups_element.innerHTML += `<p class="writeup"><a href=${data.html_url}>${data.name}</a></p>`;
     }
-};
+}
 
 request.send();
