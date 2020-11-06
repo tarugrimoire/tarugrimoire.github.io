050283585237a9ff1b179b40e10692d77f25efc9
taru <taruru1010@gmail.com>
2020-11-06 12:19:23 +0900

    github apiを叩いて、レポジトリ名を取得しHTMLに反映させるスクリプト の追加

---
+++ script.js
@@ -0,0 +1,21 @@
+var request = new XMLHttpRequest();
+ 
+request.open('GET', 'https://api.github.com/orgs/tarugrimoire/repos', true);
+request.responseType = 'json';
+
+var writeups_element = document.getElementsByClassName("writeups")[0];
+
+request.onload = function () {
+  var api_resp = this.response;
+  for (let i = 0; i < api_resp.length; i++) {
+      const data = api_resp[i];
+      console.log(data)
+      if (data.name == "tarugrimoire.github.io") {
+          continue
+      }
+      writeups_element.innerHTML += `<p class="writeup"><a href=${data.html_url}>${data.name}</a></p>`
+      
+    }
+};
+
+request.send();
